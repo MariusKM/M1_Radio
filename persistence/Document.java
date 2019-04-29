31402acb9e3a6c025d070283c377a07a9346c779
Anna B <Anna.Bespalov@student.htw-berlin.de>
2019-04-29 10:30:39 +0200

    getter/setter added

--- persistence/Document.java
+++ persistence/Document.java
@@ -1,11 +1,54 @@
 package de.sb.radio.persistence;
 
-public class Document extends BaseEntity{
+public class Document extends BaseEntity {
 	private String hash;
 	private byte[] content;
-    private String type;
-	
-	//char[] hash = new char[64];
-    // byte[] content = new byte[16777215];
- 
+	private String type;
+
+	public Document() {
+
+	}
+
+	/**
+	 * @return the hash
+	 */
+	public String getHash() {
+		return hash;
+	}
+
+	/**
+	 * @param hash the hash to set
+	 */
+	protected void setHash(String hash) {
+		this.hash = hash;
+	}
+
+	/**
+	 * @return the content
+	 */
+	public byte[] getContent() {
+		return content;
+	}
+
+	/**
+	 * @param content the content to set
+	 */
+	protected void setContent(byte[] content) {
+		this.content = content;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	protected void setType(String type) {
+		this.type = type;
+	}
+
 }
