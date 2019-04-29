3fb9ee5e31c3fd8c1708e62f27f6b63c2d056294
Odalys <odalys.adam@posteo.de>
2019-04-29 13:13:41 +0200

    added some annotations

--- persistence/Address.java
+++ persistence/Address.java
@@ -1,8 +1,16 @@
 package de.sb.radio.persistence;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 public class Address {
+	@NotNull @NotEmpty @Size(min=0, max=63)
 	private String street;
+	@NotNull @NotEmpty @Size(min=0, max=15) @Positive
 	private String postcode;
+	@NotNull @NotEmpty @Size(min=1, max=63)
 	private String city;
 	
 	public Address(String street, String postcode, String city) {
